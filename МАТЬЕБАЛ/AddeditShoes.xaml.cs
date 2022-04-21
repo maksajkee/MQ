ad5387d1c87de1b3f707cd5d082abbbdc5b7f08f
User <User@DESKTOP-FTPG4U2>
2022-04-21 06:31:55 +0300

    Добавьте файлы проекта.

---
+++ МАТЬЕБАЛ/AddeditShoes.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace МАТЬЕБАЛ
+{
+    /// <summary>
+    /// Логика взаимодействия для AddeditShoes.xaml
+    /// </summary>
+    public partial class AddeditShoes : Page
+    {
+        public AddeditShoes()
+        {
+            InitializeComponent();
+        }
+    }
+}
